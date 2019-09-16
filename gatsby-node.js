248c5183a431a7b7aec158f2d461961e1ebd50f9
Sallar Kaboli <sallar@kaboli.org>
2019-09-16 14:02:27 +0300

    Initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack")
+
+const CSS_PATTERN = /\.css$/
+const MODULE_CSS_PATTERN = /\.module\.css$/
+
+const isCssRules = rule =>
+  rule.test &&
+  (rule.test.toString() === CSS_PATTERN.toString() ||
+    rule.test.toString() === MODULE_CSS_PATTERN.toString())
+
+const findCssRules = config =>
+  config.module.rules.find(
+    rule => Array.isArray(rule.oneOf) && rule.oneOf.every(isCssRules)
+  )
+
+exports.onCreateWebpackConfig = ({
+  getConfig,
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  const config = getConfig()
+  const cssRules = findCssRules(config)
+
+  if (cssRules) {
+    cssRules.oneOf.forEach(statement => {
+      statement.use = statement.use.map(item => {
+        if (
+          item.loader.match(/\/css-loader\//) &&
+          item.options.modules === true
+        ) {
+          item.loader = "typings-for-css-modules-loader"
+          item.options = {
+            ...item.options,
+            namedExport: true,
+          }
+        }
+        return item
+      })
+    })
+  }
+
+  config.plugins.push(new webpack.WatchIgnorePlugin([/css\.d\.ts$/]))
+
+  actions.replaceWebpackConfig(config)
+}
